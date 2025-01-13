7e82764fc90014aca7353dcdf379c21974b503ae
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-01-14 04:24:36 +0530

    After connected DB

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
+import './config/instrument.js'
 import express from "express";
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./config/db.js";
+import * as Sentry from "@sentry/node";
 
 
 
 //initialize express
 const app=express()
+import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 //connect to databse
 await connectDB()
@@ -20,9 +24,21 @@ app.use(express.json())
 //routes
 app.get('/',(req,res)=>res.send('API Working'))
 
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
+  
+app.post('/webhooks',clerkWebhooks)
+
 
 //Port
 const PORT=process.env.PORT || 5000
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
