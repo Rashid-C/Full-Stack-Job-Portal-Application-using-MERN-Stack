99959929f5bfad32d7d120141cdde7aeb8bf9344
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-01-18 03:07:38 +0530

    basic logic of getUserData

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,29 @@
+import User from "../models/User.js";
 
 //get user data
-export const getUserData=async(req,res)=>{
-  
-}
+export const getUserData = async (req, res) => {
+  const userId = req.auth.userId; //clerk auth with frondent
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    res.json({ success: true, user });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
 
 // apply for a job
-export const applyForJob=async(req,res)=>{
-
-}
+export const applyForJob = async (req, res) => {
+    
+};
 
 //get user applied applications
-export const getUserJobApplication=async(req,res)=>{
-
-}
+export const getUserJobApplication = async (req, res) => {};
 
 //update user profile(resume)
-export const updateUserResume=async(req,res)=>{
-    
-}
+export const updateUserResume = async (req, res) => {};
