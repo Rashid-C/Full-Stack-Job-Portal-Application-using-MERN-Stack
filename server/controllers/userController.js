e71f4067716eb30e6b9f9b0c271eabbbb938c164
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-01-18 03:26:35 +0530

    Added functinality to applyForJob

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,5 @@
+import Job from "../models/Job.js";
+import JobApplication from "../models/jobApplication.js";
 import User from "../models/User.js";
 
 //get user data
@@ -19,7 +21,34 @@ export const getUserData = async (req, res) => {
 
 // apply for a job
 export const applyForJob = async (req, res) => {
-    
+  const { jobId } = req.body;
+
+  const userId = req.auth.userId;
+
+  try {
+    const isAlreadyApplied = await JobApplication.find({ jobId, userId });
+
+    if (isAlreadyApplied.length > 0) {
+      return res.json({ success: false, message: "Already Applied" });
+    }
+
+    const jobData = await Job.findById(jobId);
+
+    if (!jobData) {
+      return res.json({ success: false, message: "Job Not Found" });
+    }
+
+    await JobApplication.create({
+      companyId: jobData.companyId,
+      userId,
+      jobId,
+      date: Date.now(),
+    });
+
+    return res.json({ success: true, message: "Applied successfully" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
 };
 
 //get user applied applications
