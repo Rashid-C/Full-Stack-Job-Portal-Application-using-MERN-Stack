4a920433f9e87c3c6f65e7790615f839de223a67
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-01-23 02:32:12 +0530

    notification when data is empty

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -74,64 +74,74 @@ const ManageJobs = () => {
     }
   }, [companyToken]);
 
-  return jobs ? jobs.length===0?(<div></div>) :(
-    <div className="container p-4 max-w-5xl">
-      <div className="overflow-x-auto">
-        <table className="min-w-full bg-white border border-gray-200 max-sm:text-sm">
-          <thead>
-            <tr>
-              <th className="py-2 px-4 border-b text-left max-sm:hidden">#</th>
-              <th className="py-2 px-4 border-b text-left">Job Title</th>
-              <th className="py-2 px-4 border-b text-left max-sm:hidden">
-                Date
-              </th>
-              <th className="py-2 px-4 border-b text-left max-sm:hidden">
-                Location
-              </th>
-              <th className="py-2 px-4 border-b text-center">Applicants</th>
-              <th className="py-2 px-4 border-b text-left">Visible</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Array.isArray(jobs) &&
-              jobs.map((job, index) => (
-                <tr key={index} className="text-gray-700">
-                  <td className="py-2 px-2 border-b max-sm:hidden">
-                    {index + 1}
-                  </td>
-                  <td className="py-2 px-2 border-b">{job.title}</td>
-                  <td className="py-2 px-2 border-b max-sm:hidden">
-                    {formatDate(job.date)}
-                  </td>
-                  <td className="py-2 px-2 border-b max-sm:hidden">
-                    {job.location}
-                  </td>
-                  <td className="py-2 px-2 border-b text-center">
-                    {job.applicants}
-                  </td>
-                  <td className="py-2 px-2 border-b">
-                    <input
-                      onChange={() => changeJobVisibility(job._id)}
-                      className="scale-125 ml-4"
-                      type="checkbox"
-                      checked={job.visible}
-                    />
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
+  return jobs ? (
+    jobs.length === 0 ? (
+      <div className="flex items-center justify-center h-[70vh]">
+        <p className="text-xl sm:text-2xl ">No Jobs Available or Posted</p>
       </div>
-      <div className="mt-4 flex justify-end">
-        <button
-          onClick={() => navigate("/dashboard/add-job")}
-          className="bg-black text-white py-2 px-4 rounded"
-        >
-          Add new job
-        </button>
+    ) : (
+      <div className="container p-4 max-w-5xl">
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white border border-gray-200 max-sm:text-sm">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b text-left max-sm:hidden">
+                  #
+                </th>
+                <th className="py-2 px-4 border-b text-left">Job Title</th>
+                <th className="py-2 px-4 border-b text-left max-sm:hidden">
+                  Date
+                </th>
+                <th className="py-2 px-4 border-b text-left max-sm:hidden">
+                  Location
+                </th>
+                <th className="py-2 px-4 border-b text-center">Applicants</th>
+                <th className="py-2 px-4 border-b text-left">Visible</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.isArray(jobs) &&
+                jobs.map((job, index) => (
+                  <tr key={index} className="text-gray-700">
+                    <td className="py-2 px-2 border-b max-sm:hidden">
+                      {index + 1}
+                    </td>
+                    <td className="py-2 px-2 border-b">{job.title}</td>
+                    <td className="py-2 px-2 border-b max-sm:hidden">
+                      {formatDate(job.date)}
+                    </td>
+                    <td className="py-2 px-2 border-b max-sm:hidden">
+                      {job.location}
+                    </td>
+                    <td className="py-2 px-2 border-b text-center">
+                      {job.applicants}
+                    </td>
+                    <td className="py-2 px-2 border-b">
+                      <input
+                        onChange={() => changeJobVisibility(job._id)}
+                        className="scale-125 ml-4"
+                        type="checkbox"
+                        checked={job.visible}
+                      />
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="mt-4 flex justify-end">
+          <button
+            onClick={() => navigate("/dashboard/add-job")}
+            className="bg-black text-white py-2 px-4 rounded"
+          >
+            Add new job
+          </button>
+        </div>
       </div>
-    </div>
-  ):<Loading />
-}
+    )
+  ) : (
+    <Loading />
+  );
+};
 
 export default ManageJobs;
--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -58,7 +58,9 @@ const ViewApplications = () => {
 
   return applicats ? (
     applicats.length === 0 ? (
-      <div></div>
+      <div className="flex items-center justify-center h-[70vh]">
+        <p className="text-xl sm:text-2xl ">No Applications Available</p>
+      </div>
     ) : (
       <div className="container mx-auto p-4">
         <div>
